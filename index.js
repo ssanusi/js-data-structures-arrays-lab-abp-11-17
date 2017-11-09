0f0206c9b63f8e6984e13684354268effd087aae
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-09 19:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift();
 }
 function appendDriver(name){
-  return drivers.splice(3,0,name);
+  return drivers.splice(drivers.length-1,0,name);
 }
